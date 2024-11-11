3cfbbcb93b5e3eb708009235b9d4395162250dd5
IvenCraft <53281184+IvenCraft@users.noreply.github.com>
2024-11-11 15:21:26 +0100

    Ini Commit

---
+++ src/test/java/de/itch/multimedia/MultimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.itch.multimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
