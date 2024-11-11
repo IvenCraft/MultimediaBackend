a7be96e1dcf47a349d5b6c5f5b9c52601047463a
IvenCraft <53281184+IvenCraft@users.noreply.github.com>
2024-11-11 16:35:09 +0100

    Swagger Eingeführt

--- src/main/java/de/itch/multimedia/MultimediaApplication.java
+++ src/main/java/de/itch/multimedia/MultimediaApplication.java
@@ -1,9 +1,12 @@
 package de.itch.multimedia;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Multimedia Screen", version = "1.0", description = "API für den Multimedia Screen der Immo Eppendorf GmbH <br><br> Erstellt im LF2 der Itch Hamburg"))
 public class MultimediaApplication {
 
 	public static void main(String[] args) {
